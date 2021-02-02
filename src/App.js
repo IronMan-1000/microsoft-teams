bd5905576df83f082a6397587f552fc854215868
Mauro Avellaneda <mauroeavellaneda@gmail.com>
2021-02-01 23:34:29 -0300

    Adds dependencies

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-function App() {
-  return <div className="App"></div>;
-}
+import React from "react";
+import { ChatEngine } from "react-chat-engine";
+import "./App.css";
+
+const App = () => {
+  return <div></div>;
+};
 
 export default App;
