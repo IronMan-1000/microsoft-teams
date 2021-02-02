2fc84464658ff150f3da297bb4b6371c7018b1b8
Mauro Avellaneda <mauroeavellaneda@gmail.com>
2021-02-02 18:38:43 -0300

    Adds TheirMessage componend and passing message and last message to render

--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from "react";
 
-const TheirMessage = () => {
-    return (
-        <div>
-            Their Message
+const TheirMessage = ({ lastMessage, message }) => {
+  const isFirstMessageByUser =
+    !lastMessage || lastMessage.sender.username !== message.sender.username;
+  return (
+    <div className="message-row">
+      {isFirstMessageByUser && (
+        <div
+          className="message-avatar"
+          style={{ backgroundImage: `url(${message?.sender?.avatar})` }}
+        />
+      )}
+      {message?.attachments?.length > 0 ? (
+        <img
+          src={message.attachments[0].file}
+          alt="message-attachment"
+          className="message-image"
+          style={{ float: "right" }}
+        />
+      ) : (
+        <div
+          className="message"
+          style={{
+            float: "right",
+            marginRight: "18px",
+            backgroundColor: "#3B2A50",
+            color: "white",
+          }}
+        >
+          {message.text}
         </div>
-    )
-}
+      )}
+    </div>
+  );
+};
 
-export default TheirMessage
+export default TheirMessage;
