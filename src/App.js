98ba901b3a46721bcf8500dbfabd4a33e2d017cd
Mauro Avellaneda <mauroeavellaneda@gmail.com>
2021-02-01 23:59:40 -0300

    Adds env variables and configures chat engine

--- src/App.js
+++ src/App.js
@@ -2,8 +2,18 @@ import React from "react";
 import { ChatEngine } from "react-chat-engine";
 import "./App.css";
 
+const API_KEY = process.env.REACT_APP_CHAT_ENGINE;
+const API_PASS = process.env.REACT_APP_CHAT_ENGINE_PASSWORD;
+
 const App = () => {
-  return <div></div>;
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={API_KEY}
+      userName="mauroavellaneda"
+      userSecret={API_PASS}
+    />
+  );
 };
 
 export default App;
